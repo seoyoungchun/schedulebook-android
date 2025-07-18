08b5b64e1e9f9906b3eb4a3d255b9be8bbdef334
seoyoungchun <seoyc17@gmail.com>
2025-07-19 00:41:57 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "일정표_가계부"
+include(":app")
+include(":app:data")
+include(":app:repository")
+include(":app:viewmodel")
+include(":app:ui")
